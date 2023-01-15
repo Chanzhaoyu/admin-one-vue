5feadf878d432fe05da04311b8e05367139185bd
ChenZhaoYu <790348264@qq.com>
2023-01-15 10:55:38 +0800

    feat: some pages

---
+++ src/layouts/LayoutAuthenticated.vue
@@ -0,0 +1,102 @@
+<script setup lang="ts">
+import { mdiForwardburger, mdiBackburger, mdiMenu } from "@mdi/js";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import menuAside from "@/menuAside";
+import menuNavBar from "@/menuNavBar";
+import { useMainStore } from "@/stores/main";
+import { useStyleStore } from "@/stores/style";
+import BaseIcon from "@/components/BaseIcon.vue";
+import FormControl from "@/components/FormControl.vue";
+import NavBar from "@/components/NavBar.vue";
+import NavBarItemPlain from "@/components/NavBarItemPlain.vue";
+import AsideMenu from "@/components/AsideMenu.vue";
+import FooterBar from "@/components/FooterBar.vue";
+
+useMainStore().setUser({
+  name: "ZhaoYu Chan",
+  email: "chenzhaoyu1994@gmail.com",
+  avatar: "https://avatars.githubusercontent.com/u/24789441?v=4",
+});
+
+const layoutAsidePadding = "xl:pl-60";
+
+const styleStore = useStyleStore();
+
+const router = useRouter();
+
+const isAsideMobileExpanded = ref(false);
+const isAsideLgActive = ref(false);
+
+router.beforeEach(() => {
+  isAsideMobileExpanded.value = false;
+  isAsideLgActive.value = false;
+});
+
+const menuClick = () => {};
+</script>
+
+<template>
+  <div
+    :class="{
+      dark: styleStore.darkMode,
+      'overflow-hidden lg:overflow-visible': isAsideMobileExpanded,
+    }"
+  >
+    <div
+      :class="[layoutAsidePadding, { 'ml-60 lg:ml-0': isAsideMobileExpanded }]"
+      class="pt-14 min-h-screen w-screen transition-position lg:w-auto bg-gray-50 dark:bg-slate-800 dark:text-slate-100"
+    >
+      <NavBar
+        :menu="menuNavBar"
+        :class="[
+          layoutAsidePadding,
+          { 'ml-60 lg:ml-0': isAsideMobileExpanded },
+        ]"
+        @menu-click="menuClick"
+      >
+        <NavBarItemPlain
+          display="flex lg:hidden"
+          @click.prevent="isAsideMobileExpanded = !isAsideMobileExpanded"
+        >
+          <BaseIcon
+            :path="isAsideMobileExpanded ? mdiBackburger : mdiForwardburger"
+            size="24"
+          />
+        </NavBarItemPlain>
+        <NavBarItemPlain
+          display="hidden lg:flex xl:hidden"
+          @click.prevent="isAsideLgActive = true"
+        >
+          <BaseIcon :path="mdiMenu" size="24" />
+        </NavBarItemPlain>
+        <NavBarItemPlain use-margin>
+          <FormControl
+            placeholder="Search (ctrl+k)"
+            ctrl-k-focus
+            transparent
+            borderless
+          />
+        </NavBarItemPlain>
+      </NavBar>
+      <AsideMenu
+        :is-aside-mobile-expanded="isAsideMobileExpanded"
+        :is-aside-lg-active="isAsideLgActive"
+        :menu="menuAside"
+        @menu-click="menuClick"
+        @aside-lg-close-click="isAsideLgActive = false"
+      />
+      <slot />
+      <FooterBar>
+        Get
+        <a
+          href="https://github.com/Chanzhaoyu/admin-one-vue"
+          target="_blank"
+          class="text-blue-600"
+        >
+          Github
+        </a>
+      </FooterBar>
+    </div>
+  </div>
+</template>
