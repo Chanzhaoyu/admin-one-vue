829ad49d919b3ea841f5db738eef93cde06f912f
ChenZhaoYu <790348264@qq.com>
2023-01-15 16:40:11 +0800

    chore: lint

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,13 +1,4 @@
-<script setup lang="ts">
-import { computed, ref } from "vue";
-import { useMainStore } from "@/stores/main";
-import { mdiCheckDecagram } from "@mdi/js";
-import BaseLevel from "@/components/BaseLevel";
-import UserAvatarCurrentUser from "@/components/UserAvatarCurrentUser.vue";
-import CardBox from "@/components/CardBox.vue";
-import FormCheckRadio from "@/components/FormCheckRadio.vue";
-import PillTag from "@/components/PillTag.vue";
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div></div>
