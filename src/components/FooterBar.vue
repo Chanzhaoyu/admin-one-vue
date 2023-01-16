6c9e211330eece0e565b599c1c82c2fff8de8310
ChenZhaoYu <790348264@qq.com>
2023-01-16 09:39:26 +0800

    feat: Footer

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,5 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { containerMaxW } from "@/config.js";
+import BaseLevel from "@/components/BaseLevel";
+
+const year = new Date().getFullYear();
+</script>
 
 <template>
-  <div></div>
+  <footer class="py-2 px-6" :class="containerMaxW">
+    <BaseLevel>
+      <div class="text-center md:text-left">
+        <b>&copy;{{ year }}</b>
+        <slot />
+      </div>
+    </BaseLevel>
+  </footer>
 </template>
