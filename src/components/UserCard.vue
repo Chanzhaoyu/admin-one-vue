32071049214ff979ca35ddf74a102ce78933d760
ChenZhaoYu <790348264@qq.com>
2023-01-16 14:06:33 +0800

    feat: Profile View

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,5 +1,42 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed, ref } from "vue";
+import { useMainStore } from "@/stores/main";
+import { mdiCheckDecagram } from "@mdi/js";
+import BaseLevel from "@/components/BaseLevel";
+import UserAvatarCurrentUser from "@/components/UserAvatarCurrentUser.vue";
+import CardBox from "@/components/CardBox.vue";
+import FormCheckRadio from "@/components/FormCheckRadio.vue";
+import PillTag from "@/components/PillTag.vue";
+
+const mainStore = useMainStore();
+
+const userName = computed(() => mainStore.userName);
+
+const userSwitchVal = ref(false);
+</script>
 
 <template>
-  <div></div>
+  <CardBox>
+    <BaseLevel type="justify-around lg:justify-center">
+      <UserAvatarCurrentUser class="w-[200px] lg:mx-12" />
+      <div class="space-y-3 text-center md:text-left lg:mx-12">
+        <div class="flex justify-center md:block">
+          <FormCheckRadio
+            v-model="userSwitchVal"
+            name="notifications-switch"
+            type="switch"
+            label="Notifications"
+            :input-value="true"
+          />
+        </div>
+        <h1 class="text-2xl">
+          <b>{{ userName }}</b>
+        </h1>
+        <p>Last login <b>12 mins ago</b> from <b>127.0.0.1</b></p>
+        <div class="flex justify-center md:block">
+          <PillTag label="Verified" color="info" :icon="mdiCheckDecagram" />
+        </div>
+      </div>
+    </BaseLevel>
+  </CardBox>
 </template>
