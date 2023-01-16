22e31744f3b5b3192d887ab335b99c313b483fb9
ChenZhaoYu <790348264@qq.com>
2023-01-16 10:14:45 +0800

    feat: login page

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,27 @@
-<script setup lang="ts">
+<script lang="ts" setup>
+import SectionFullScreen from "@/components/SectionFullScreen.vue";
+import CardBox from "@/components/CardBox.vue";
+import BaseButton from "@/components/BaseButton.vue";
+import BaseButtons from "@/components/BaseButtons";
 import LayoutGuest from "@/layouts/LayoutGuest.vue";
 </script>
 
 <template>
-  <LayoutGuest> </LayoutGuest>
+  <LayoutGuest>
+    <SectionFullScreen v-slot="{ cardClass }" bg="purplePink">
+      <CardBox :class="cardClass">
+        <div class="space-y-3">
+          <h1 class="text-2xl">Login Page</h1>
+
+          <p></p>
+        </div>
+
+        <template #footer>
+          <BaseButtons>
+            <BaseButton label="Back" to="/dashboard" color="info" />
+          </BaseButtons>
+        </template>
+      </CardBox>
+    </SectionFullScreen>
+  </LayoutGuest>
 </template>
